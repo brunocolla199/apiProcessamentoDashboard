d1f1fe7509a8bbea906dca0faa3739cdf2d31430
Lucas Zonin <lucaszsoares1@hotmail.com>
2022-01-24 23:58:22 -0300

    recebimento das informacoes ok, integrando com ged agora

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
 from flask import Flask, request
 from flask_cors import CORS 
-import json, requests, base64
+import json, requests, base64, ast
 from io import StringIO
 from datetime import date, datetime
 
+#set FLASK_APP = app.py
+#set FLASK_ENV = development
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=['POST'])
-def dashboard():
+def recebendo_informacoes():
     
-    print(request.data)
+    #Recebendo o JSON e byte-data e transformando para dicionario python
+    variaveis_recebidas = ast.literal_eval(request.data.decode('UTF-8'))
+
+    #Variaveis para fazer requisicoes para o GED
+    token         = variaveis_recebidas['token']
+    url           = f"{variaveis_recebidas['url']}/registro/pesquisa"
+    areas         = variaveis_recebidas['body']['listaIdArea']
+    indice_target = variaveis_recebidas['body']['indiceArea']
+
+    # tipo_grafico = variaveis_recebidas['body']['tipoGrafico']
+    # datas = variaveis_recebidas['body']['datas']
+
+    headers = {"Cookie" : f"CXSSID={token}",
+               "content-type" : "application/json"
+             }
+
+    body_ged = {
+        "listaIdArea": areas,
+        "listaIndice": [
+            f"{indice_target}"
+        ],
+        "inicio": 0,
+        "fim": 1000
+    }
+
+    extraindo_informacoes_ged(url, body_ged, headers)
+
     return request.data
 
+
+
+
+def extraindo_informacoes_ged(url, body, headers):
+
+    body_ged = json.dumps(body, ensure_ascii=False)
+    response_ged = requests.post(url, headers=headers, data=body_ged)
+    print(response_ged.content)
 
 if __name__ == "__main__":
     app.run()
