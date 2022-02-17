d163a064179d19ed4dba2393a23c6c72500ab5e9
Lucas Zonin <lucaszsoares1@hotmail.com>
2022-02-16 21:48:05 -0300

    regras de negocio aplicadas, removi o label do dashboard 'nome_data=Dez-2021'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #Flask
-from flask import Flask, request, abort
+from flask import Flask, Response, request, abort, render_template
 from flask_cors import CORS 
 
 #Outros
@@ -32,39 +32,38 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/grafico", methods=['POST'])
-#Nesta função eu apenas recebo informacoes do WeeHealth e preparo para fazer a requisicao dos dados la no GED
+#Receber informacoes do WeeHealth e preparo para fazer a requisicao dos dados la no GED
 def main():
 
-    #Recebendo os dados como formulário
+    #Receber os dados como formulário do Weehealth
     try: 
         
         variaveis_recebidas = request.form.to_dict(flat=False)
-
         token_target  = variaveis_recebidas['token'][0]
         url_target    = "{}/registro/pesquisa".format(variaveis_recebidas['url'][0])
 
         json_recebido = json.loads(variaveis_recebidas['body'][0])
-
-        area_target   = json_recebido['listaIdArea']
-        indice_target = json_recebido['indiceArea']
-        datas_target  = json_recebido['datas']
-        tipo_grafico  = json_recebido['tipoGrafico']
-
+        area_target           = json_recebido['listaIdArea'] #Id da área que contém os dados
+        indice_target         = json_recebido['indiceArea']  #Coluna no GED que o usuário quer ver no dashboard
+        datas_target          = json_recebido['datas']       #Período que o usuário quer
+        tipo_grafico          = json_recebido['tipoGrafico'] #Tipo do gráfico que o usuário quer
+        indices_filtro_target = json_recebido['indiceValor'] #Esses índices podem vir com valor ou apenas uma lista vazia     
+        
     except:
 
         logging.error('As informações não foram enviadas corretamente. Tente novamente! BAD REQUEST - 400')
         abort(400)   
 
 
-    #Preparando os HEADERS
+    #Preparar os HEADERS
     headers = {"Cookie" : "CXSSID={}".format(token_target),
                "content-type" : "application/json"
              }
 
     #Extração dos dados do GED
-    dataframe_dados_ged = extraindo_informacoes_ged(url_target, area_target, headers, indice_target, datas_target)
+    dataframe_dados_ged = preparacao_extracao_dados(url_target, area_target, headers, indice_target, datas_target, indices_filtro_target)
 
-    #Chamo o método pra criar o gráfico
+    #Chamar o método para criar o gráfico
     grafico = criando_dashboard(dataframe_dados_ged, indice_target, tipo_grafico)
     
     try: 
@@ -78,24 +77,17 @@ def main():
        logging.error('Ocorreu um erro ao renderizar o grafico. Verifique o tipo do grafico que foi passado e tente novamente! BAD REQUEST - 400')
        return abort(400)
 
-#Recebo a URL, body, headers, indice recebido do weehealth e as datas recebidas do weehealth
-def extraindo_informacoes_ged(url, area_target, headers, indice_target, datas_target):
+#Receber a URL, body, headers, indice recebido do weehealth e as datas recebidas do weehealth
+def preparacao_extracao_dados(url, area_target, headers, indice_target, datas_target, indices_filtro_target):
 
-    #Aplicando paginação
+    #Aplicar paginação
 
     inicio = 0
-    fim = 5000 #Escolhi esse número porque o Zyad disse que é o limite do GED
+    fim = 5000 #Esse número foi escolhido porque o Zyad disse que é o limite do GED
     
-
-    #Estruturando os dados recebidos pelo WeeHealth
+    #Dataframe utilizado para estruturar todos os dados para criar os dashboards
     dataframe = pd.DataFrame()
-    datas_target_dataframe                = pd.DataFrame(datas_target)
-    datas_target_dataframe['dataInicial'] = pd.to_datetime(datas_target_dataframe['dataInicial'])
-    datas_target_dataframe['dataFinal']   = pd.to_datetime(datas_target_dataframe['dataFinal'])
-
-    data_target_minima = datas_target_dataframe['dataInicial'].min()
-    data_target_maxima = datas_target_dataframe['dataFinal'].max()
-
+    
     #Início dos loops para adicionar dados ao dataframe
     while True: 
 
@@ -111,40 +103,65 @@ def extraindo_informacoes_ged(url, area_target, headers, indice_target, datas_ta
         response_ged = requests.post(url, headers=headers, data=body_ged)
         retorno_ged = json.loads(response_ged.content.decode('utf-8'))
 
-        #Preparo o dataframe com os registros do Indice e as datas dos registros
+        #Preparar o dataframe com os registros do Indice e as datas dos registros
         for registro in retorno_ged['listaRegistro']:
 
             chave_valor = {}
-            
+           
             for indice in registro['listaIndice']:
                 
-                
-                #Buscando a data do registro no GED
+                #Buscar a data do registro no GED
                 chave_valor[indice['identificador']] = indice['valor']
-            
-            #Pode ser que a coluna não exista na área especificada no GED
-            try: 
+
+            dataframe = dataframe.append(chave_valor, ignore_index=True)
+
+        #Transformações nos dados
+        
+        #Estruturar os dados para filtragem, recebidos pelo WeeHealth - Datas
+        datas_target_dataframe                = pd.DataFrame(datas_target)
+        datas_target_dataframe['dataInicial'] = pd.to_datetime(datas_target_dataframe['dataInicial'])
+        datas_target_dataframe['dataFinal']   = pd.to_datetime(datas_target_dataframe['dataFinal'])
+        data_target_minima = datas_target_dataframe['dataInicial'].min()
+        data_target_maxima = datas_target_dataframe['dataFinal'].max()
+
+        #Estruturar os dados para filtragem, recebidos pelo Weehealth - Outras informações
+        indices_fitro_target_dataframe = pd.DataFrame(indices_filtro_target)
+
+
+        dataframe['Data_do_registro'] = pd.to_datetime(dataframe['Data_do_registro'], dayfirst=True)
+
+        #FILTROS NO DATAFRAME
+
+        #Filtrar o período
+        dataframe = dataframe[(dataframe['Data_do_registro'] >= data_target_minima) & (dataframe['Data_do_registro'] <= data_target_maxima)]
+
+        #Verificar se o dataframe dos filtros não está vazio. Filtrar pelos indices que o Weehealth passa em indiceValor
+        if not indices_fitro_target_dataframe.empty:
+
+            for indice_valor in indices_fitro_target_dataframe[['indice', 'valor']].iterrows():
+
+                indice = indice_valor[1]['indice']
+                valor = indice_valor[1]['valor']
                 
-                data_registro = pd.to_datetime(chave_valor['Data_do_registro'], dayfirst=True)
-                
-                #Filtro o período recebido pelo WeeHealth com os dados do GED
-                if (data_registro >= data_target_minima) & (data_registro <= data_target_maxima):
+                dataframe = dataframe[dataframe[indice] == valor]
 
-                    dataframe = dataframe.append({
-                        indice_target : chave_valor[indice_target],
-                        'data_registro' : data_registro
-                    }, ignore_index=True)
-                
-                else:
+        #Filtar pelo indice target que o Weehealth envia 
+        try:
 
-                    continue 
+            dataframe = dataframe[[indice_target, 'Data_do_registro']]
 
-            except:
+        except:
 
-                logging.error('Nao existe a coluna {} nessa area do GED. Tente novamente!'.format(indice_target))
-                return abort(400)
+            logging.error('Não existe a coluna passada como parâmetro no GED. BAD REQUEST - 400')
+            return abort(400)
 
-        #Busco a quantidade de loops dividindo a quantidade de fim pelo resultado da pesquisa e arrendondo pra cima.
+
+        if dataframe.empty:
+
+            logging.error('Não há registros com o filtros selecionados!')
+            return abort(400)
+
+        #Buscar a quantidade de loops dividindo a quantidade de fim pelo resultado da pesquisa e arrendondo pra cima.
         quantidade_loops = (retorno_ged['totalResultadoPesquisa'] / fim)
         quantidade_loops = math.ceil(quantidade_loops)
 
@@ -158,10 +175,10 @@ def extraindo_informacoes_ged(url, area_target, headers, indice_target, datas_ta
         else:
 
             break 
-        
-    #Busco o nome da data recebido da aplicação da Weehealth para cada data do registro
+            
+    #Buscar o nome da data recebido da aplicação da Weehealth para cada data do registro
     lista_nome_datas = []
-    for data_registro in dataframe['data_registro']:
+    for data_registro in dataframe['Data_do_registro']:
         for datas in datas_target_dataframe.iterrows():
 
             #Filtro pelas datas recebidas do weehealth com as datas do GED
@@ -176,21 +193,26 @@ def extraindo_informacoes_ged(url, area_target, headers, indice_target, datas_ta
 #Passar dados para construir dashboard
 def criando_dashboard(dados_dashboard, indice_target, tipo_grafico):
 
-    #Agrupando os dados para gerar a contagem de registros
-    dados_agrupados_data_indice = dados_dashboard.groupby(['nome_data', indice_target]).count().reset_index().rename(columns={'data_registro' : 'contagem'})
+    #Agrupar os dados para gerar a contagem de registros
+    dados_agrupados_data_indice = dados_dashboard.groupby(['nome_data', indice_target]).count().reset_index().rename(columns={'Data_do_registro' : 'contagem'})
 
-    ##Verificando qual tipo de gráfico a aplicação da weehealth quer
+    ##Verificar qual tipo de gráfico a aplicação da weehealth quer
 
     #Gráfico de linhas
     if tipo_grafico == '1':
         
+        #Fazer em ordem do mês
         fig = px.line(dados_agrupados_data_indice, x="nome_data", y="contagem", color=indice_target, markers=True, title='Dados referentes a {}'.format(indice_target))
         return fig
 
-    #Gráfico de barras
+    #Gráfico de barras agrupado por nome da data
     elif tipo_grafico == '2':
 
-        fig = px.bar(dados_agrupados_data_indice, x=indice_target, y='contagem', barmode="group", facet_col='nome_data', title='Dados referentes a {}'.format(indice_target))
+        #Fazer em ordem do mês
+        fig = px.bar(dados_agrupados_data_indice, x=indice_target, y='contagem', barmode="group", 
+                    facet_col='nome_data', title='Dados referentes a {}'.format(indice_target))
+        fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+
         return fig
 
     #Gráfico de pizza
@@ -202,7 +224,7 @@ def criando_dashboard(dados_dashboard, indice_target, tipo_grafico):
     #Totalizador (A ideia é agrupar só pelo indice_target e ignorar a data)
     elif tipo_grafico == '4':
         
-        #Conto a quantidade total de cada registro
+        #Contagem da quantidade total de cada registro
         dados_agrupados_indice = dados_dashboard[indice_target].value_counts().reset_index(name='contagem').rename(columns={'index' : indice_target})
         
         fig = px.bar(dados_agrupados_indice, x=indice_target, y='contagem', title='Totalizador dos dados referentes a {}'.format(indice_target))
